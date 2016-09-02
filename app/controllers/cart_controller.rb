6b2aca3b2a7edb6349d1558e36916be413f905da
jabox <jabox2@gmail.com>
2016-09-02 01:46:52 +0000

    Fix view order action

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -66,50 +66,49 @@ class CartController < ApplicationController
       flash[:alert] = "Order is empty"
       redirect_to root_path
     else
-    
-    #sets total quantities for each item in order and sets order.subtotal
-    @order.subtotal = 0
-    @line_items.each do |line_item|
-      if @order.order_items[line_item.product_id].nil?
-        @order.order_items[line_item.product_id] = line_item.quantity
-      else
-        @order.order_items[line_item.product_id] += line_item.quantity
-      end
-      
-      @order.subtotal += line_item.line_item_total
-    end
-    
-    @order.sales_tax = @order.subtotal * 0.08
-    @order.grand_total = @order.subtotal + @order.sales_tax
-    @order.save
-    
-    if out_of_stock #returns true if order can be filled
+      #sets total quantities for each item in order and sets order.subtotal
+      @order.subtotal = 0
       @line_items.each do |line_item|
-      #to prevent overselling an item
-        line_item.product.quantity -= line_item.quantity
-        line_item.product.save
-      end
-
-      @order.line_items.destroy_all
-      @order.save
-    else #what to do if item is out of stock
-      messages = ''
-      first = true
-      @out_of_stock.each do |product_id, short|
-        product = Product.find(product_id)
-        if first
-          messages = "We are short #{short} #{product.name}"
-          first = false
+        if @order.order_items[line_item.product_id].nil?
+          @order.order_items[line_item.product_id] = line_item.quantity
         else
-          messages += ", and #{short} #{product.name}"
+          @order.order_items[line_item.product_id] += line_item.quantity
         end
+        
+        @order.subtotal += line_item.line_item_total
       end
-      messages += " for your order."
       
-      flash[:alert] = messages
-      redirect_to :view_order
+      @order.sales_tax = @order.subtotal * 0.08
+      @order.grand_total = @order.subtotal + @order.sales_tax
+      @order.save
+      
+      if out_of_stock #returns true if order can be filled
+        @line_items.each do |line_item|
+        #to prevent overselling an item
+          line_item.product.quantity -= line_item.quantity
+          line_item.product.save
+        end
+  
+        @order.line_items.destroy_all
+        @order.save
+      else #what to do if item is out of stock
+        messages = ''
+        first = true
+        @out_of_stock.each do |product_id, short|
+          product = Product.find(product_id)
+          if first
+            messages = "We are short #{short} #{product.name}"
+            first = false
+          else
+            messages += ", and #{short} #{product.name}"
+          end
+        end
+        messages += " for your order."
+        
+        flash[:alert] = messages
+        redirect_to :view_order
+      end
     end
-  end
   end
   
   def order_complete
@@ -158,7 +157,8 @@ class CartController < ApplicationController
         user.orders << order
         user.active_order_id = order.id
       else
-        user.active_order_id = user.orders.first
+        order = user.orders.first
+        user.active_order_id = order.id
       end
       user.save
     else
